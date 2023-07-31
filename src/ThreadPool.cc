181b276fddd353169c194aaad4104c58ccfb9aa3
Simpenzwe Honore Leandre <leandrehonore650@gmail.com>
2023-07-31 17:50:50 +0200

    threadpool implemented

--- src/ThreadPool.cc
+++ src/ThreadPool.cc
@@ -20,11 +20,42 @@ class ThreadPool
                    std::function<void()> task;
                    {
                        std::unique_lock<std::mutex> lock(queueMutex); 
-                       
-    
+                       //wait until queue is not empty or done is true
+                       this->condition.wait(lock, [this]{return this->done || !this->tasks.empty();});
+                       if(this->done and this->tasks.empty())
+                       {
+                            //exit loop and thread, destructor will be called
+                            return;
+                       }
+                       task = std::move(this->tasks.front());
+                       this->tasks.pop();                           
                    }
+                    task();
                 }
             });
+        }
+    }
+
+    void queueWork(std::function<void()> task)
+    {
+        {
+            //aquire lock and push task into queue
+            std::unique_lock<std::mutex> lock(queueMutex);
+            this->tasks.emplace(std::move(task));
+        }
+        condition.notify_one();
+    }
+
+    ~ThreadPool()
+    {
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+            done = true;
+        }
+        condition.notify_all();
+        for(std::thread& thread:threads)
+        {
+            thread.join();
         }
     }
 
@@ -34,8 +65,5 @@ class ThreadPool
     std::queue<std::function<void()>> tasks;
     std::mutex queueMutex;
     std::condition_variable condition;
-
-    
-    
 
 };
